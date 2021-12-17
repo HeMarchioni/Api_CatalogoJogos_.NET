c561c7b9a12e53b11edb855c303aab484b3be80d
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-12-17 03:08:37 -0300

    Jogo input e view Model

---
+++ ApiCatalogoJogos/ViewModel/JogoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ApiCatalogoJogos.ViewModel
+{
+    public class JogoViewModel
+    {
+
+        public Guid Id { get; set; }  //-> tipo GUID é um número inteiro de 128 bits (16 bytes) que você pode usar em todos os computadores e redes sempre que um identificador exclusivo for necessário.
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+
+
+    }
+}
