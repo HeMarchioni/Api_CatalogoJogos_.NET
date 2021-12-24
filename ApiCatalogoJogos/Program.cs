8c9799bad4a9311d0d95fd69f8be6b6c507471ee
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-12-24 03:08:33 -0300

    Migrando - .NET 5 para .NET 6

--- ApiCatalogoJogos/Program.cs
+++ ApiCatalogoJogos/Program.cs
@@ -1,26 +1,62 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using ApiCatalogoJogos.Middleware;
+using ApiCatalogoJogos.Repositories;
+using ApiCatalogoJogos.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
-namespace ApiCatalogoJogos
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExemploApiCatalogoJogos", Version = "v1" });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    var basePath = AppDomain.CurrentDomain.BaseDirectory;
+    var fileName = typeof(Program).GetTypeInfo().Assembly.GetName().Name + ".xml";
+    c.IncludeXmlComments(Path.Combine(basePath, fileName));
+});
+
+
+builder.Services.AddScoped<IJogoService, JogoService>();   // -> injeção de dependecia do construtor para poder intanciar e trabalhar com a interface (que vai representar a classe definida)
+builder.Services.AddScoped<IJogoRepository, JogoSqlServerRepository>();
+
+
+
+// CicloDeVida  (Tipos de injeção de Dependencia)
+
+//services.AddSingleton<IExemploSingleton, ExemploCicloDeVida>();    --> É criada uma unica instancia ela se mantem assim que a aplicação é iniciada
+//services.AddScoped<IExemploScoped, ExemploCicloDeVida>();          --> É criada uma instancia unica toda vez q a rota do controlador é chamada depois é fechada
+//services.AddTransient<IExemploTransient, ExemploCicloDeVida>();    --> É criada uma intancia para cada chamada individualmete dentro do controlador depois fechada
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiCatalogoJogos v1"));
 }
+
+
+app.UseMiddleware<ExceptionMiddleware>();  // -> Para Utilizar o Middleware que eu criei.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
